00f05a988f03aec61034e0a802a991bffcc1c016
BRUHItsABunny <53124399+BRUHItsABunny@users.noreply.github.com>
2026-03-19 16:55:41 -0500

    feat: additional http2 settings

--- http2_opt.go
+++ http2_opt.go
@@ -7,12 +7,14 @@ import (
 )
 
 type HTTP2ParametersOption struct {
-	HeaderTableSize       int64
-	EnablePush            int64
-	MaxConcurrentStreams  int64
-	InitialWindowSize     int64
-	MaxFrameSize          int64
-	MaxHeaderListSize     int64
+	HeaderTableSize      int64
+	EnablePush           int64
+	MaxConcurrentStreams int64
+	InitialWindowSize    int64
+	MaxFrameSize         int64
+	MaxHeaderListSize    int64
+	AdditionalValues     []int64
+
 	WindowUpdateIncrement uint32
 
 	HTTP2PriorityFrameSettings *oohttp.HTTP2PriorityFrameSettings
@@ -22,14 +24,15 @@ func (o *HTTP2ParametersOption) Execute(client *http.Client) error {
 	typedTrans, ok := client.Transport.(*oohttp.StdlibTransport)
 	if ok {
 		typedTrans.Transport.HasCustomInitialSettings = true
-		typedTrans.Transport.HTTP2SettingsFrameParameters = []int64{
-			o.HeaderTableSize,
-			o.EnablePush,
-			o.MaxConcurrentStreams,
-			o.InitialWindowSize,
-			o.MaxFrameSize,
-			o.MaxHeaderListSize,
-		}
+		typedTrans.Transport.HTTP2SettingsFrameParameters = append(
+			[]int64{
+				o.HeaderTableSize,
+				o.EnablePush,
+				o.MaxConcurrentStreams,
+				o.InitialWindowSize,
+				o.MaxFrameSize,
+				o.MaxHeaderListSize,
+			}, o.AdditionalValues...)
 
 		typedTrans.Transport.HasCustomWindowUpdate = true
 		typedTrans.Transport.WindowUpdateIncrement = o.WindowUpdateIncrement
